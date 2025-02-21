aa6b2fb09ad1a112b3e304b9dfd00da3d2fc5052
akhlaque <akhlaque.ak@gmail.com>
2025-02-20 19:28:28 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -733,6 +733,7 @@ else{
 				if(j == new_n) j = i+1;
 				if(j >= vp.size()||ub > best_sz||ub + vp.size() - j <= best_sz) break;
 				ui dj = ids[j];
+				assert(dj<R_end - S_end);
 				ui u = vp[dj].first;
 				ui nn = vp[ids[j]].second;
 				if(t_support < nn) break;
