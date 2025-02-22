0cd89bc42795dda27dc62a6386f9b9384c1c68c9
akhlaque <akhlaque.ak@gmail.com>
2025-02-22 15:23:43 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -1434,11 +1434,11 @@ private:
 		for (; R_end < old_R_end; R_end++)
 		{ // insert u back into R
 			ui u = SR[R_end];
-			// if(SR_rid[u]!=R_end){
-			// 	for(ui i=0;i<R_end;i++) cout<<SR_rid[SR[i]]<< " ";
-			// 	cout<<": ";
-			// 	cout<<SR_rid[u]<<" "<<R_end<<" " << old_R_end<<endl;
-			// } 
+			if(SR_rid[u]!=R_end){
+				for(ui i=0;i<R_end;i++) cout<<SR_rid[SR[i]]<< " ";
+				cout<<": ";
+				cout<<SR_rid[u]<<" "<<R_end<<" " << old_R_end<<endl;
+			} 
 			assert(SR_rid[u] == R_end);
 			assert(level_id[u] == level);
 			level_id[u] = n;
