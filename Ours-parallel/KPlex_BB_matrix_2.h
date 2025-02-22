0ee6b108c08b814b1cc691901f665cbb7fcac29a
akhlaque <akhlaque.ak@gmail.com>
2025-02-21 23:00:21 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -886,6 +886,8 @@ private:
 					BB_search(S_end, R_end, level + 1, false, false, st);
 				// the second branch exclude u from G
 				restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
+
+			}
 				B->clear();
 				while (!Qv.empty()){
 					Qv.pop();
@@ -898,8 +900,6 @@ private:
 					BB_search(S_end, R_end, level + 1, false, false, st);
 				}
 				restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
-
-			}
 		}
 		restore_SR_and_edges(S_end, R_end, old_S_end, old_R_end, level, old_removed_edges_n);
 	}
