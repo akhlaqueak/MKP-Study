3dfb4d10eeebcee241b78afc73a4c74d7ae2af95
akhlaque <akhlaque.ak@gmail.com>
2025-02-21 21:00:23 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -850,7 +850,6 @@ private:
 					}
 					if (td->move_u_to_S_with_prune(u, S_end, R_end, level));
 						td->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
-
 					td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 					td->deallocate();
 				}
@@ -867,7 +866,8 @@ private:
 					}
 					td1->Qv.push(u);
 					td1->level_id[u] = level;
-					bool succeed = td1->collect_removable_vertices_and_edges(S_end, R_end, level);
+					bool succeed=true;
+					// bool succeed = td1->collect_removable_vertices_and_edges(S_end, R_end, level);
 					if (succeed&&td1->remove_vertices_and_edges_with_prune(S_end, R_end, level))
 						td1->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
 					td1->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
