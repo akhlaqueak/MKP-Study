179ce75420d4ac57e1991aa17ddb77fab217fc1a
akhlaque <akhlaque.ak@gmail.com>
2025-02-23 01:43:21 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -1449,7 +1449,7 @@ private:
 		for (; R_end < old_R_end; R_end++)
 		{ // insert u back into R
 			ui u = SR[R_end];
-
+			SR_rid[u] = R_end;
 			// assert(SR_rid[u] == R_end);
 			// assert(level_id[u] == level);
 			level_id[u] = n;
