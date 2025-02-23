9fcbddb4de332ef632094a4159daa3d7c5a33eb3
akhlaque <akhlaque.ak@gmail.com>
2025-02-23 01:13:09 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -870,15 +870,15 @@ private:
 
 				B.clear();
 				{
-				ui pre_best_solution_size = best_solution_size.load(), t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
-				empty_Qv();
-				Qv.push(u);
-				level_id[u] = level;
-				// bool succeed = collect_removable_vertices_and_edges(S_end, R_end, level);
-				if (remove_vertices_and_edges_with_prune(S_end, R_end, level)){
-					BB_search(S_end, R_end, level + 1, false, false, st);
-				}
-				// restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
+					ui pre_best_solution_size = best_solution_size.load(), t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
+					empty_Qv();
+					Qv.push(u);
+					level_id[u] = level;
+					bool succeed = collect_removable_vertices_and_edges(S_end, R_end, level);
+					if (succeed&&remove_vertices_and_edges_with_prune(S_end, R_end, level)){
+						BB_search(S_end, R_end, level + 1, false, false, st);
+					}
+					restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 				}
 			}
 		}
