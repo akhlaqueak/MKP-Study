edc1b8af062fc7f093ce4103caed1fba76c49a79
akhlaque <akhlaque.ak@gmail.com>
2025-02-23 20:29:00 -0500

    updating

--- Ours-parallel/KPlex_BB_matrix_2.h
+++ Ours-parallel/KPlex_BB_matrix_2.h
@@ -293,7 +293,7 @@ public:
 		PIMax = new vector<ui>();
 		ISc = new vector<ui>();
 		ids = new vector<ui>();
-		
+
 		B.reserve(n);
 		PI->reserve(n);
 		PIMax->reserve(n);
@@ -895,22 +895,27 @@ private:
 				{
 					KPLEX_BB_MATRIX *td = new KPLEX_BB_MATRIX();
 					td->loadContext(solvers[omp_get_thread_num()], ctx, R_end);
-					delete ctx; ctx=nullptr;
+					delete ctx;
+					ctx = nullptr;
 					assert(td->SR_rid[u] < R_end && td->SR_rid[u] >= S_end);
 					// First branch moves u to S
 					ui pre_best_solution_size = best_solution_size.load(), t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
 					td->empty_Qv();
-					if (td->move_u_to_S_with_prune(u, S_end, R_end, level));
-					td->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
+					if (td->move_u_to_S_with_prune(u, S_end, R_end, level))
+						td->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
 					td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
-					// }
-					// B.clear();
-					// KPLEX_BB_MATRIX *ctx1 = new KPLEX_BB_MATRIX(*this, R_end);
+					delete td;
+					td = nullptr;
+				}
+				B.clear();
+				KPLEX_BB_MATRIX *ctx1 = new KPLEX_BB_MATRIX(*this, R_end);
 
-					// #pragma omp task firstprivate(ctx, u, S_end, R_end, level)
-					// 				{
-					// 					KPLEX_BB_MATRIX *td = new KPLEX_BB_MATRIX();
-					// 					td->loadContext(solvers[omp_get_thread_num()], ctx, R_end);
+#pragma omp task firstprivate(ctx1, u, S_end, R_end, level)
+				{
+					KPLEX_BB_MATRIX *td = new KPLEX_BB_MATRIX();
+					td->loadContext(solvers[omp_get_thread_num()], ctx1, R_end);
+					delete ctx1;
+					ctx1 = nullptr;
 					assert(td->SR_rid[u] < R_end && td->SR_rid[u] >= S_end);
 					td->B.clear();
 					// ui pre_best_solution_size = best_solution_size.load(), t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
@@ -927,6 +932,7 @@ private:
 					td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 					td->nullify();
 					delete td;
+					td = nullptr;
 				}
 				restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 			}
