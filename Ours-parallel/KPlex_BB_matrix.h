544ce98bdfcb9e491934d05b8708f64f769042af
akhlaque <akhlaque.ak@gmail.com>
2025-03-11 12:19:31 -0400

    updating

--- Ours-parallel/KPlex_BB_matrix.h
+++ Ours-parallel/KPlex_BB_matrix.h
@@ -5,7 +5,7 @@
 #include "Timer.h"
 #include <chrono>
 using namespace std::chrono;
-#define _SECOND_ORDER_PRUNING_
+// #define _SECOND_ORDER_PRUNING_
 #define THRESH 100
 #define TIME_NOW chrono::steady_clock::now()
 #define TIME_OVER(ST) ((chrono::duration_cast<chrono::microseconds>(TIME_NOW - ST)).count() > THRESH)
@@ -834,7 +834,7 @@ private:
 						ui t_old_S_end = S_end, t_old_R_end = R_end, t_old_removed_edges_n = 0;
 						if (td->move_u_to_S_with_prune(u, S_end, R_end, level))
 							td->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
-						// td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
+						td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 						
 					}
 				}
@@ -878,7 +878,7 @@ private:
 					td->empty_Qv();
 					if (td->move_u_to_S_with_prune(u, S_end, R_end, level))
 						td->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
-					// td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
+					td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 					td->nullify();
 					delete td;
 					td = nullptr;
@@ -905,7 +905,7 @@ private:
 						assert(t_old_R_end > R_end);
 						td->BB_search(S_end, R_end, level + 1, false, false, TIME_NOW);
 					}
-					// td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
+					td->restore_SR_and_edges(S_end, R_end, t_old_S_end, t_old_R_end, level, t_old_removed_edges_n);
 					td->nullify();
 					delete td;
 					td = nullptr;
