63078ecdedf11826ce5f1c4d6ebc38186b00487a
akhlaque <akhlaque.ak@gmail.com>
2025-03-20 10:37:39 -0400

    updating

--- Ours-serial/main.cpp
+++ Ours-serial/main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 #define LEN_LIMIT (1<<10)
 char filename[LEN_LIMIT];
 
+#define DENSE_SEARCH
+
 int main(int argc, char *argv[]) {
 	printf("\n-----------------------------------------------------------------------------------------\n");
 	if (argc == 3) {
+	#ifndef DENSE_SEARCH
 		strncpy(filename, argv[1], LEN_LIMIT);
 		int k = atoi(argv[2]);
 		Graph *graph = new Graph(filename, k);
@@ -16,8 +19,7 @@ int main(int argc, char *argv[]) {
 		graph->search();
 		graph->write();
 		// delete graph; // there are some bugs in releasing memory
-	}
-	else if (argc == 4) {
+	#else
 		strncpy(filename, argv[1], LEN_LIMIT);
 		int k = atoi(argv[2]);
 		Graph *graph = new Graph(filename, k);
@@ -26,6 +28,7 @@ int main(int argc, char *argv[]) {
 		graph->search_dense();
 		graph->write();
 		// delete graph; // there are some bugs in releasing memory
+	#endif
 	}
 	else printf("[usage]: exe file k [dense]\n");
 	printf("-----------------------------------------------------------------------------------------\n\n");
